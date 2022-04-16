cd7cc1f8a966ea13713da4c04f155bdf7f222a67
robmanganelly <robmanganelly@gmail.com>
2022-04-16 02:36:08 -0400

    creating static routes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const  path = require('path');
 const  cookieParser = require('cookie-parser');
 const  logger = require('morgan');
 
-const  indexRouter = require('./routes/index');
-const  usersRouter = require('./routes/users');
+const  routes = require('./routes/routes');
 
 const  app = express();
 
@@ -15,9 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'images')));
+app.use('/main',express.static(path.join(__dirname, 'public', 'html')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
